a55539737d0e01717bea5da9f6e3f9518528c24d
Luke-Noel <120382667+Luke-Noel@users.noreply.github.com>
2023-04-27 02:31:19 +0100

    Misery

--- GorillaWarfare/Assets/Assignment Assets/Luke's/Scripts/Menu/MainMenu.cs
+++ GorillaWarfare/Assets/Assignment Assets/Luke's/Scripts/Menu/MainMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void Play()
-   {
+    public void Play()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
+    }
 
     public void Quit()
     {
         Application.Quit();
-        Debug.Log("PLayer Quit");
+        Debug.Log("Player has quit");
     }
 }
